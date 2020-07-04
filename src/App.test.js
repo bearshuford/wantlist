a7e9d219420bb5945cb3c47dd04f490352925c12
Bear <bearshuford@me.com>
2020-07-04 19:05:31 -0400

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders heading', () => {
+test('renders label', () => {
   const { getByText } = render(<App />);
-  const headerElement = getByText(/Wantlist/i);
+  const headerElement = getByText(/discogs username/i);
   expect(headerElement).toBeInTheDocument();
 });
