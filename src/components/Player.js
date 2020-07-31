58dd7028cfc7ae9ed6bfc859e4b6b3ad504b130a
Bear <bearshuford@me.com>
2020-07-30 21:02:35 -0400

    styled-components fix to work with nextjs

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled, { css } from "styled-components";
 import ReactPlayer from "react-player";
 
@@ -6,6 +6,8 @@ import { PlayerContext } from "../PlayerContext";
 
 const PlayIcon = () => <>&#x25ba;&#xfe0e;</>;
 const PauseIcon = () => <>&#9612;&#9612;</>;
+const DownIcon = () => <>&bigtriangledown;</>;
+const UpIcon = () => <>&bigtriangleup;</>;
 
 const StyledControls = styled.div`
   display: ${(props) => (props.hasVideo ? "flex" : "none")};
@@ -21,31 +23,31 @@ const StyledControls = styled.div`
   padding: 0 18px;
   border-top: 4px solid violet;
 
-
-  @media(min-width: 1024px) {
+  @media (min-width: 1024px) {
     left: unset;
     width: 352px;
     right: 28px;
-    border-left: 4px solid violet;
-    border-right: 4px solid violet;
+    border: 4px solid violet;
   }
 
-  @media(min-width: 1400px) {
+  @media (min-width: 1400px) {
     right: 76px;
     width: 360px;
   }
 
-  @media(min-width: 1800px) {
+  @media (min-width: 1800px) {
     right: 28%;
     width: 380px;
   }
-
 `;
 
-const StyledTumbnail = styled.img`
-  height: 64px;
-  border-radius: 2px;
+const StyledCollapseButton = styled.button`
+  background: transparent;
+  padding: 4px;
+  font-size: 20px;
   margin: 0 18px 0 0;
+  border: none;
+  box-shadow: none;
 `;
 
 const StyledTitle = styled.h4`
@@ -69,7 +71,7 @@ const StyledPlayPause = styled.button`
   margin-left: 18px;
   margin-right: 24px;
 
-  @media(min-width: 1024px) {
+  @media (min-width: 1024px) {
     margin-right: 12px;
   }
 
@@ -86,14 +88,22 @@ const StyledIframeWrapper = styled.div`
 
 function Player() {
   const { playing, setPlaying, video } = useContext(PlayerContext);
+  const [collapsed, setCollapsed] = useState(true);
 
   const togglePlayPause = () => {
     setPlaying(!playing);
   };
+
+  const toggleCollpased = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <>
       <StyledControls hasVideo={!!video}>
-        <StyledTumbnail src={video.thumbnail} />
+        <StyledCollapseButton onClick={toggleCollpased}>
+          {collapsed ? <DownIcon /> : <UpIcon />}
+        </StyledCollapseButton>
         <StyledTitle>{video.title}</StyledTitle>
         <StyledPlayPause onClick={togglePlayPause} isPlaying={playing}>
           {!!playing ? <PauseIcon /> : <PlayIcon />}
